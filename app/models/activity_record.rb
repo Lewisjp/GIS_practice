8f6cc03e27c504aab9447df194349bc9c826a10a
LewisJPaul <lewis.jp@gmail.com>
2014-05-29 00:32:47 -0400

    initial 19:00

--- app/models/activity_record.rb
+++ app/models/activity_record.rb
@@ -1,2 +1,14 @@
 class ActivityRecord < ActiveRecord::Base
+  attr_accessible :coords
+
+  GEOFACTORY = RGEO::Geographic.simple_mercator_factory
+  set_rgeo_factory_for_column(:coords, GEOFACTORY.projection_factory)
+
+  def self.in_rect(w,s,e,n)
+    sw = GEOFACTORY.point(w,s).projection
+    ne = GEOFACTORY.point(e,n).projection 
+    box = RGeo::Cartesian::BoundingBox.create_from_points(sw,ne)
+    where(["coords && ?", box])
+  end
+
 end
--- db/migrate/20140529024239_create_activity_records.rb
+++ db/migrate/20140529024239_create_activity_records.rb
@@ -1,9 +1,10 @@
 class CreateActivityRecords < ActiveRecord::Migration
   def change
     create_table :activity_records do |t|
-      t.point :coords
-
-      t.timestamps
+      t.point :coords, :srid => 3785
+    end
+    change_table :check_ins do |t|
+      t.index :coords, :spatial => true
     end
   end
 end
